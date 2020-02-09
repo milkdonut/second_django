133e8ced21f7ae5da7f8cde84686d101b97eee20
milkdonut <574062832@qq.com>
2020-02-09 17:08:44 +0800

    定制site

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from typeidea.custom_site import custom_site
+
 from .adminforms import PostAdminForm
-from .models import Post, Category, Tag
+from .models import Category, Post, Tag
 
 
-@admin.register(Category)
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
+
     list_display = ['name', 'status', 'is_nav', 'created_time', 'post_count']
     fields = ('name', 'status', 'is_nav')
 
@@ -20,7 +30,7 @@ class CategoryAdmin(admin.ModelAdmin):
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
@@ -45,7 +55,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
 
@@ -89,7 +99,7 @@ class PostAdmin(admin.ModelAdmin):
     )
 
     def operator(self, obj):
-        return format_html('<a href="{}">编辑<a/>', reverse('admin:blog_post_change', args=(obj.id,)))
+        return format_html('<a href="{}">编辑<a/>', reverse('cus_admin:blog_post_change', args=(obj.id,)))
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
