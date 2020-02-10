923a29037318768c9aaef7f1a3193e913513a00e
milkdonut <574062832@qq.com>
2020-02-10 15:39:30 +0800

    抽象admin基类

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
 
 from .adminforms import PostAdminForm
@@ -15,7 +16,7 @@ class PostInline(admin.TabularInline):
 
 
 @admin.register(Category, site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseOwnerAdmin):
     inlines = [PostInline, ]
 
     list_display = ['name', 'status', 'is_nav', 'created_time', 'post_count']
@@ -25,19 +26,19 @@ class CategoryAdmin(admin.ModelAdmin):
         return obj.post_set.count()
     post_count.short_description = '文章数量'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Tag, site=custom_site)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
@@ -56,7 +57,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
 
     list_display = ('title', 'category', 'status',
@@ -100,15 +101,16 @@ class PostAdmin(admin.ModelAdmin):
 
     def operator(self, obj):
         return format_html('<a href="{}">编辑<a/>', reverse('cus_admin:blog_post_change', args=(obj.id,)))
+        
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super(PostAdmin, self).save_model(request, obj, form, change)
 
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
+    # def get_queryset(self, request):
+    #     qs = super(PostAdmin, self).get_queryset(request)
+    #     return qs.filter(owner=request.user)
 
     # class Media:
     #     css = {
--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
 
 from .models import Comment
 
 
 @admin.register(Comment, site=custom_site)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BaseOwnerAdmin):
     list_display = ['target', 'nickname', 'content', 'website', 'created_time']
