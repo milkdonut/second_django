72215bd9350c93a3b5381950e6bc3ecb0e29fb54
milkdonut <574062832@qq.com>
2020-02-14 17:42:44 +0800

    升级至class-base view

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list, post_detail
+from blog.views import IndexView, CategoryView, TagView, PostDetailView
 from config.views import links
 from .custom_site import custom_site
 
 urlpatterns = [
-    path('', post_list, name='index'),
-    path('category/<int:category_id>/', post_list, name='category-list'),
-    path('tag/<int:tag_id>/', post_list, name='tag-list'),
-    path('post/<int:post_id>.html', post_detail, name='post-detail'),
+    path('', IndexView.as_view(), name='index'),
+    path('category/<int:category_id>/', CategoryView.as_view(), name='category-list'),
+    path('tag/<int:tag_id>/', TagView.as_view(), name='tag-list'),
+    path('post/<int:post_id>.html', PostDetailView.as_view(), name='post-detail'),
     path('links/', links, name='links'),
     path('super_admin/', admin.site.urls, name='super-admin'),
     path('admin/', custom_site.urls, name='admin'),
